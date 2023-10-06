ba93d58b0865a40559bf36739bda78eb04547944
rifkydarmawan62 <146593199+rifkydarmawan62@users.noreply.github.com>
2023-10-07 02:02:51 +0700

    Update __init__.py

--- Modul/metode_peretasan/__init__.py
+++ Modul/metode_peretasan/__init__.py
@@ -1,4 +1,8 @@
-"Memperbolehkan anda untuk memilih berbagai metode peretasan yang tersedia"
+"""
+Memperbolehkan anda untuk memilih berbagai metode peretasan yang tersedia.
+Namun perlu di ingat ini adalah modul yang tidak dapat digunakan untuk peretasan secara langsung.
+Anda harus membuat kode pemrograman untuk peretasan anda sendiri dan pastikan anda tidak menggunakan modul ini untuk tujuan ilegal!
+"""
 import typing
 from tkinter import filedialog
 
