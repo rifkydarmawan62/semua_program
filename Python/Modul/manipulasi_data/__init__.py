4a5def48b4ce37a48b1df5c176e607fb77dc454e
rifkydarmawan62 <rifkydarmawan62@gmail.com>
2023-12-05 01:35:16 +0700

    Optimalisasi Kode Program

--- Python/Modul/manipulasi_data/__init__.py
+++ Python/Modul/manipulasi_data/__init__.py
@@ -1,6 +1,6 @@
-from typing import Collection, Optional, Iterable, Any
+from typing import Optional, Iterable, Any
 
-PEMBARUAN_TERAKHIR = "23 November 2023"
+PEMBARUAN_TERAKHIR = "5 Desember 2023"
 "Tanggal program terakhir kali diperbarui"
 
 #Program binary search masih terdapat kemungkinan bug
@@ -25,45 +25,42 @@ def binary_search(data : Collection[int], __boolean_output : bool = False, *, da
         return False
 """
 class tumpukan:
-    __DATA : list = []
+    DATA : list = []
     def __init__(self, tumpukan_baru : Optional[Iterable | Any], *, maks_tumpuk : Optional[int]):
         "Buat objek tumpukan baru"
         assert maks_tumpuk > 0, f"Maks tumpuk {maks_tumpuk} harus lebih dari nol!"
         self.MAKS_TUMPUK = maks_tumpuk
         if tumpukan_baru != None:
             if isinstance(tumpukan_baru, Iterable):
-                self.__DATA.extend(tumpukan_baru)
+                self.DATA.extend(tumpukan_baru)
             else:
-                self.__DATA.append(tumpukan_baru)
+                self.DATA.append(tumpukan_baru)
     def adalah_kosong(self) -> bool:
         "Mengembalikan True jika data yang ditumpuk kosong, False jika tidak"
-        if len(self.__DATA) == 0:
+        if len(self.DATA) == 0:
             return True
         else:
             return False
     def tambah(self, __data_baru : Iterable | Any):
         "Menambah tumpukan yang paling atas"
         if isinstance(__data_baru, Iterable):
-            self.__DATA.extend(__data_baru)
+            self.DATA.extend(__data_baru)
         else:
-            self.__DATA.append(__data_baru)
+            self.DATA.append(__data_baru)
     def ambil(self):
         "Mengambil tumpukan yang paling atas\n\nMengembalikan None jika data yang ditumpuk kosong"
-        if len(self.__DATA) > 0:
-            return self.__DATA.pop()
+        if len(self.DATA) > 0:
+            return self.DATA.pop()
         else:
             return None
-    def ambil_semua(self):
-        "Mengambil semua tumpukan"
-        return self.__DATA
     def hapus_semua(self):
         "Menghapus semua data yang ditumpuk"
-        self.__DATA.clear()
+        self.DATA.clear()
     def ambil_dan_hapus_semua(self):
         "Mengambil lalu menghapus semua data yang ditumpuk"
-        __objek_sementara = self.__DATA
-        self.__DATA.clear()
+        __objek_sementara = self.DATA
+        self.DATA.clear()
         return __objek_sementara
     def jumlah_elemen(self):
         "Mengembalikan jumlah elemen yang ditumpuk"
-        return len(self.__DATA)
+        return len(self.DATA)
